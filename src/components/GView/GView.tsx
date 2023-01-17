815e75acdb1d91c1fd980fd02b90affb13d56a77
blueBye <navidshirmohammadi81@gmail.com>
2023-01-18 01:00:28 +0330

    working

--- src/components/GView/GView.tsx
+++ src/components/GView/GView.tsx
@@ -1,23 +1,42 @@
 import * as React from 'react';
 import { subject } from '../../service.js'
+import {useEffect, useState} from 'react';
 
 
 const GView = () => {
-    const [model, setModel] = React.useState(0);
+    const [model, setModel] = useState({});
 
     subject.subscribe(res=>{
-        console.log(res)
         setModel(res)
     })
 
+    useEffect(() => {
+        setModel({asad: 12, dd: 'dd'});
+    }, []);
+
     return (
         <div id="view">
-            {Object.keys(model).map((key,i)=>{
-                return (<li key={i}>{model[key]}</li>)
+            {Object.keys(model).map((block, index)=>{
+                return (
+                <ul key={index}>{index}: {JSON.stringify(model[block])}</ul>)
             })}
         </div>
     )
+
+    // let model = {}
+
+    // subject.subscribe(res=>{
+    //     console.log(res)
+    //     this.obj = res
+    // })
+
+    // return (
+    //     <div id="view">
+    //         {Object.keys(model).map((key,i)=>{
+    //             return (<li key={i}>{model[key]}</li>)
+    //         })}
+    //     </div>
+    // )
 }
 
 export { GView };
-
